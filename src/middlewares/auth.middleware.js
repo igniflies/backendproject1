eb1ae42f80ce820969682759c24532dd11f40d64
Neha <vishishtneha65@gmail.com>
2024-12-26 13:33:43 +0530

    final

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,15 +1,15 @@
-import { ApiError } from "../utils/apiErrors";
-import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
-import { User } from "../models/user.model";
+import { User } from "../models/user.model.js";
 
-export const verifyJWT =asyncHandler(async(req,res,next)=>{
+export const verifyJWT =asyncHandler(async(req,_,next)=>{
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","")
     if(!token){
         throw new ApiError(401,"Unauthorized request")
     }
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)//is se decoded info(token) mil jaati h , ye token user ke paas gyah , it is diff from that saved in data bse
     const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     if(!user){
         throw new ApiError(401,"Invalid Access Token")
